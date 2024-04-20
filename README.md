b28d9612f5345c7a8754062d057882c342bf00c3
yikejason <18215513386@163.com>
2024-04-20 22:22:46 +0800

    parse cli params

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # RCLI
 rcli is a rust CLI tool
+
+# learn a programming language
+- learn x in Y minutes
+website: https://learnxinyminutes.com/
+
+# 学习 clap crate 基本用法
+- cargo add clap --features derive
+一个 crate 里有多个features 这里是使用 clap 中一个feature的derive功能，其他用不着的功能不用下载
+
+
+# 构建基本的 CLI 的结构
+# 使用 duckdb 查看 csv
+# 学习 csv carate 基本用法
+# 学习 serde 基本用法
+# 读取 csv，输出到console
+# 学习 serde-json 基本用法
+# 将 csv 转换成 json
+# 重构代码
+
+# question
+1. #[command(name = "rcli", version, author, about, long_about = None)] #[command()]这个宏是从哪里引入的?
+
+# vscode 小技巧
+- 选中代码 选中 rename symbol 作用域下的要改的重复东西都会被修改
+- mac command + . 快速修复代码
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::path::Path;
+// rcli csv -i input.csv -o output.csv --header -d ','
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(Debug, Parser)]
+enum Subcommand {
+    #[command(name = "csv", about = "show CSV, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+// default_value 是可以使用from trait 来进行解析
+// default_value_t 是直接设置指定类型不用转换
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    input: String,
+    //  form trait 里会干这件事情 "output.json".into()
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
+fn verify_input_file(filename: &str) -> Result<String, String> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File {} does not exist".into())
+    }
 }
